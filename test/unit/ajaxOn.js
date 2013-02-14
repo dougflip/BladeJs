724aad21dcaff26abe8560aa246d6ee8e6ab369f
Doug DiFilippo <douglasdifilippo@gmail.com>
2013-02-13 20:35:18 -0500

    Adds some basic tests for callbacks
    If nothing else, for testing purposes, I need to make gathering AJAX settings separated from calling AJAX.
    Then I will be able to verify the properties of settings without actually worrying about cancel AJAX calls.

--- test/unit/ajaxOn.js
+++ test/unit/ajaxOn.js
@@ -5,6 +5,9 @@ test('The ajaxOn method serializes a field', function() {
     deepEqual('GET', settings.type, 'Incorrect request Type was set.');
     var qs = settings.url.substring(settings.url.indexOf('?')+1);
     deepEqual('FirstName=Geddy', qs, 'Field was not properly serialized into the query string');
+
+    // need to make sure and clear this so it does not interfere with other tests!
+    $.fn.blade({beforeSend:null});
   }});
 
   $('#FirstName').blade('ajaxOn').change();
@@ -16,6 +19,9 @@ test('The ajaxOn method handles custom serialize attributes', function() {
     ok(settings !== null, 'Settings was null!');
     var qs = settings.url.substring(settings.url.indexOf('?')+1);
     deepEqual($('#UserForm').serialize(), qs, 'Form was not properly serialized');
+
+    // need to make sure and clear this so it does not interfere with other tests!
+    $.fn.blade({beforeSend:null});
     return false;
   }});
 
@@ -28,6 +34,9 @@ test('The ajaxOn method serializes a form', function() {
     ok(settings !== null, 'Settings was null!');
     deepEqual('POST', settings.type, 'Incorrect request Type was set.');
     deepEqual($('#UserForm').serialize(), settings.data, 'Form was not properly serialized');
+
+    // need to make sure and clear this so it does not interfere with other tests!
+    $.fn.blade({beforeSend:null});
   }});
 
   $('#UserForm').blade('ajaxOn').submit();
